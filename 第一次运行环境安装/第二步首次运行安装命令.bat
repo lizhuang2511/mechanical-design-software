4f84bda7e6504ca9351ef2895a0dfba827c29217
lz <2460162254@qq.com>
2024-12-03 17:03:42 +0800

    重新架构

--- 第一次运行环境安装/第二步首次运行安装命令.bat
+++ 第一次运行环境安装/第二步首次运行安装命令.bat
@@ -1,5 +1,5 @@
 cd /d %~dp0
-call pip install --use-deprecated=legacy-resolver  --no-index --find-links=\\192.168.64.9\¼¼Êõ²¿\¼ÆËãÈí¼þ\³õÊ¼ÔËÐÐÒÀÀµ°²×°\3.8pgk -r .\requirements.txt
+::call pip install --use-deprecated=legacy-resolver  --no-index --find-links=\\192.168.64.9\ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½\ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½\ï¿½ï¿½Ê¼ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½×°\3.8pgk -r .\requirements.txt
 call pip install  --no-index  -r .\requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 call jupyter contrib nbextension install --user --skip-running-check
 pause
